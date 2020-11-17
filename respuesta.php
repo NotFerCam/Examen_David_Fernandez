2eb626b287f3deb8a884a956fac8d62ceaacf194
NotFerCam <davidfercam69@gmail.com>
2020-11-17 17:13:48 +0100

    cosa chorra

--- respuesta.php
+++ respuesta.php
@@ -7,12 +7,5 @@
     $lat2 = $_REQUEST['n_lat2'];
     $long2 = $_REQUEST['n_long2'];
 
-    $ip1 = new LatLong($lat1, $long1);
-    $ip2 = new LatLong($lat2, $long2);
-    
-    $distanceCalculator = new DistanceCalculator($ip1, $ip2);
-
-    $distance = $distanceCalculator->get();
-
-    echo 'Distancia entre las dos coordenadas: ' . $distance->asMiles();
+    echo "Muy buenas"
 ?>
