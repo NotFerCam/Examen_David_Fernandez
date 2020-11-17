06302a1544f3809c6f0ccf71499f5218d3752dd2
NotFerCam <davidfercam69@gmail.com>
2020-11-17 16:58:17 +0100

    muchas cosas

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '255923b3e3bb549eebb719e157b6fa7c6f2ea8f5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '255923b3e3bb549eebb719e157b6fa7c6f2ea8f5',
+    ),
+    'ballen/distical' => 
+    array (
+      'pretty_version' => '2.0.7',
+      'version' => '2.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9383462e68fa0ce6f2be4a51ba8777545e82fe5',
+    ),
+  ),
+);
